ea7d69512f75f11dc58245962dd04143c49c3ae1
Kray-G <nagai.kazuya@gmail.com>
2021-01-21 18:22:16 +0900

    added a command of script runner.

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -1,12 +1,42 @@
 "use strict";
 
 import * as path from "path";
-import { ExtensionContext, window as Window } from "vscode";
+import * as child_process from "child_process";
+import { ExtensionContext, window as Window, commands as Commands, OutputChannel } from "vscode";
 import { LanguageClient, LanguageClientOptions, RevealOutputChannelOn, ServerOptions, TransportKind } from "vscode-languageclient";
-
-
 let client: LanguageClient;
+let outputChannel: OutputChannel;
+
+function runKinx(outputChannel: OutputChannel, filename: string, text: string) {
+    let dirname = path.dirname(filename);
+    let orgdir = process.cwd();
+    process.chdir(dirname);
+    let kinx = child_process.exec("kinx -i", (_error, stdout, stderr) => {
+        outputChannel.appendLine(stdout);
+        outputChannel.appendLine(stderr);
+    });
+    kinx.stdin?.write(text + '\n__END__');
+    kinx.stdin?.end();
+    process.chdir(orgdir);
+}
+
 export function activate(context: ExtensionContext): void {
+    context.subscriptions.push(Commands.registerCommand('kinx.run', () => {
+        let doc = Window.activeTextEditor?.document;
+        let filename = doc?.fileName;
+        if (filename != null) {
+            let text = doc?.getText();
+            if (text != null) {
+                if (outputChannel == null) {
+                    outputChannel = Window.createOutputChannel("Kinx Output");
+                }
+                outputChannel.clear();
+                outputChannel.show(true);
+                runKinx(outputChannel, filename, text);
+            }
+        }
+    }));
+
     const serverModule = context.asAbsolutePath(path.join("server", "out", "server.js"));
     const debugOptions = { execArgv: ["--nolazy", "--inspect=6009"], cwd: process.cwd() };
     const serverOptions: ServerOptions = {
